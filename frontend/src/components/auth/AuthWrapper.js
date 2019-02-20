b6652f0e106289b3c10e9d19f2186f6ebd144020
2muni <2muni@relesinc@gmail.com>
2019-02-20 19:52:41 +0900

    Auth #redux_1

--- frontend/src/components/auth/AuthWrapper.js
+++ frontend/src/components/auth/AuthWrapper.js
@@ -3,11 +3,25 @@ import { Link } from 'react-router-dom';
 import { FaFacebookSquare, FaGoogle } from 'react-icons/fa';
 import styles from './AuthWrapper.module.scss';
 
-const LoginForm = () => (
+const LoginForm = ({ email, password, handleChange, handleKeyPress }) => (
   <>
     <div className={styles.form}>
-      <input type="email" placeholder="email" />
-      <input type="password" placeholder="password" />
+      <input
+        type="email"
+        name="email"
+        placeholder="email"
+        value={email}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        value={password}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+      />
       <div className={styles.auth_button}>LOGIN</div>
       <Link to="/auth/register" className={styles.description}>
         if you don't have an account...
@@ -31,19 +45,32 @@ const LoginForm = () => (
   </>
 );
 
-const RegisterForm = () => (
+const RegisterForm = ({
+  email,
+  password,
+  birth,
+  handleChange,
+  handleKeyPress,
+}) => (
   <>
     <div className={styles.form}>
-      <input className={styles.input_coulmn} type="email" placeholder="email" />
       <input
         className={styles.input_coulmn}
-        type="password"
-        placeholder="password"
+        type="email"
+        name="email"
+        placeholder="email"
+        value={email}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
       />
       <input
         className={styles.input_coulmn}
         type="password"
-        placeholder="retype password"
+        name="password"
+        placeholder="password"
+        value={password}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
       />
       <div className={styles.date}>
         <input className={styles.input_row} type="number" placeholder="Year" />
@@ -58,13 +85,60 @@ const RegisterForm = () => (
   </>
 );
 
-const AuthWrapper = ({ kind }) => (
-  <div className={styles.auth_form}>
-    <div className={styles.title}>Hello Idea !</div>
-    <div className={styles.form_wrapper}>
-      {kind === 'login' ? <LoginForm /> : <RegisterForm />}
+const AuthWrapper = ({
+  kind,
+  onChangeInput,
+  email,
+  password,
+  birth,
+  onLogin,
+  onRegister,
+  error,
+}) => {
+  const handleChange = e => {
+    const { name, value } = e.target;
+    onChangeInput({ name, value });
+  };
+
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      switch (kind) {
+        case 'login':
+          onLogin();
+          break;
+        case 'register':
+          onRegister();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  return (
+    <div className={styles.auth_form}>
+      <div className={styles.title}>Hello Idea !</div>
+      <div className={styles.form_wrapper}>
+        {kind === 'login' ? (
+          <LoginForm
+            email={email}
+            password={password}
+            birth={birth}
+            handleChange={handleChange}
+            handleKeyPress={handleKeyPress}
+          />
+        ) : (
+          <RegisterForm
+            email={email}
+            password={password}
+            birth={birth}
+            handleChange={handleChange}
+            handleKeyPress={handleKeyPress}
+          />
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AuthWrapper;
--- frontend/src/containers/AuthContainer.js
+++ frontend/src/containers/AuthContainer.js
@@ -1,13 +1,90 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { AuthWrapper } from '../components/auth';
+import * as authActions from '../store/modules/auth';
 
 export class AuthContainer extends Component {
-  render() {
-    const { kind } = this.props;
+  componentDidMount() {
+    this.initialize();
+  }
 
-    return <AuthWrapper kind={kind} />;
+  componentDidUpdate(prevProps, prevState) {
+    const { history, kind } = this.props;
+    if (prevProps.kind !== kind) {
+      this.initialize();
+    }
+  }
+
+  initialize = () => {
+    const { initializeInput, initializeError } = this.props;
+    initializeError();
+    initializeInput();
+  };
+
+  handleChangeInput = ({ name, value }) => {
+    const { changeInput } = this.props;
+    changeInput({ name, value });
+  };
+
+  handleLogin = () => {
+    const { login } = this.props;
+    login();
+  };
+
+  handleRegister = () => {
+    const { register } = this.props;
+    register();
+  };
+
+  render() {
+    const { kind, email, password, birth, error } = this.props;
+    const { handleChangeInput, handleLogin, handleRegister } = this;
+
+    return (
+      <AuthWrapper
+        kind={kind}
+        email={email}
+        password={password}
+        birth={birth}
+        onChangeInput={handleChangeInput}
+        onLogin={handleLogin}
+        onRegister={handleRegister}
+        error={error}
+      />
+    );
   }
 }
 
-export default withRouter(AuthContainer);
+const mapStateToProps = state => ({
+  email: state.auth.form.email,
+  password: state.auth.form.password,
+  birth: state.auth.form.birth,
+  error: state.auth.error,
+  logged: state.auth.logged,
+});
+
+const mapDispatchToProps = dispatch => ({
+  initializeInput: () => {
+    dispatch(authActions.initializeInput());
+  },
+  changeInput: ({ name, value }) => {
+    dispatch(authActions.changeInput({ name, value }));
+  },
+  initializeError: () => {
+    dispatch(authActions.initializeError());
+  },
+  register: () => {
+    dispatch(authActions.register());
+  },
+  login: () => {
+    dispatch(authActions.login());
+  },
+});
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(AuthContainer),
+);
